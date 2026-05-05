8ce9bd2e14208a05e5abdaeb1f50780e588eddf2
Kaua <kaua7santos7oliveiraa@gmailcom>
2026-05-05 11:25:36 -0300

    autenticação feita

---
+++ app/auth.py
@@ -0,0 +1,67 @@
+# 1. Hash e verificação de senhas bcrypt
+# 2. Gerção do token JWT
+# 3. Leitura e validação do token vindo do cookiie
+
+from datetime import datetime, timedelta, timezone
+from jose import JWSError, jwt
+from passlib.context import CryptContext
+from fastapi import Request, HTTPException, status
+from dotenv import load_dotenv
+import os
+
+# Carregar as variaveis de ambiente
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRAÇAO_MINUTOS = os.getenv("ACCESS_TOKEN_EXPIRAÇAO_MINUTOS")
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+# funçoes de senha 
+def hash_senha(senha: str):
+    return pwd_context.hash(senha)
+
+def verificar_senha(senha: str, senha_hash: str):
+    return pwd_context.verify(senha, senha_hash)
+
+# funçoes do token 
+def criar_token(data: dict):
+    
+    payload = data.copy()
+    # Define quando o token expira 
+    expira = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRAÇAO_MINUTOS)
+    payload.update({"exp": expira})
+
+    # criar o token 
+    token = jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
+    return token
+
+def decodificar_token(token: str):
+    payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+    return payload
+
+# dependencia fastapi
+def get_usuario_logado(request: Request):
+    token = request.cookies.get("acess_token")
+    
+    if not token:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Nao autenticado"
+        )
+    try:
+        payload = decodificar_token(token)
+        email = payload.get("sub")
+
+        if email is None:
+            raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="token invalido")
+        
+        return payload 
+    except JWSError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Token invalido ou expirado"
+        )
